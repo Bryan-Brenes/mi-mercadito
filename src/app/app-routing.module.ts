1e56b94a220224fbc4494921a34d7e232a8cb3f9
marcasro9 <37092548+marcasro9@users.noreply.github.com>
2020-01-16 15:45:00 -0600

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -22,37 +22,3 @@ const routes: Routes = [
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
-
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-import { LoginComponent } from './controlAcceso/login/login.component';
-import { RegistroComponent } from './controlAcceso/registro/registro.component';
-import { RegistroComercianteComponent } from './controlAcceso/registro-comerciante/registro-comerciante.component';
-import { NavbarClienteComponent } from './cliente/navbar-cliente/navbar-cliente.component';
-import { NavbarCarritoClienteComponent } from './cliente/navbar-carrito-cliente/navbar-carrito-cliente.component';
-import { PerfilClienteComponent } from './cliente/perfil-cliente/perfil-cliente.component';
-import { CarritoClienteComponent } from './cliente/carrito-cliente/carrito-cliente.component';
-import { PagoClienteComponent } from './cliente/pago-cliente/pago-cliente.component';
-import { FacturacionClienteComponent } from './cliente/facturacion-cliente/facturacion-cliente.component';
-import { VerPuestosClienteComponent } from './cliente/ver-puestos-cliente/ver-puestos-cliente.component';
-import { BuscarProductosClienteComponent } from './cliente/buscar-productos-cliente/buscar-productos-cliente.component';
-
-
-
-const routes: Routes = [
-  { path: '', component: LoginComponent },
-  { path: 'registroComprador', component: RegistroComponent },
-  { path: 'registroComerciante', component: RegistroComercianteComponent },
-  { path: 'cliente', component: PerfilClienteComponent },
-  { path: 'cliente/carrito', component: CarritoClienteComponent },
-  { path: 'cliente/metodoPago', component: PagoClienteComponent },
-  { path: 'cliente/facturacion', component: FacturacionClienteComponent },
-  { path: 'cliente/verPuesto', component: VerPuestosClienteComponent},
-  { path: 'cliente/buscarProducto', component: BuscarProductosClienteComponent },
-];
-
-@NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
-})
-export class AppRoutingModule { }

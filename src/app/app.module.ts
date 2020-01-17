3cf02eae55b6d0fb6d0f24847302b837862a4dd2
Bryan Brenes <bryanbrenes1@gmail.com>
2020-01-16 18:45:23 -0600

    arreglo module detalle compra

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -24,6 +24,7 @@ import { ItemPedidosComponent } from './cliente/item-pedidos/item-pedidos.compon
 import { VerDetallePuestoClienteComponent } from './cliente/ver-detalle-puesto-cliente/ver-detalle-puesto-cliente.component';
 import { ItemComentarioComponent } from './comerciante/item-comentario/item-comentario.component';
 import { InventarioComercianteComponent } from './comerciante/inventario-comerciante/inventario-comerciante.component';
+import { DetalleCompraClienteComponent } from './cliente/detalle-compra-cliente/detalle-compra-cliente.component';
 
 @NgModule({
   declarations: [
@@ -48,7 +49,8 @@ import { InventarioComercianteComponent } from './comerciante/inventario-comerci
     ItemPedidosComponent,
     VerDetallePuestoClienteComponent,
     ItemComentarioComponent,
-    InventarioComercianteComponent
+    InventarioComercianteComponent,
+    DetalleCompraClienteComponent
   ],
   imports: [
     BrowserModule,
